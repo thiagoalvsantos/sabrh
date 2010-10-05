5f91fd153510efddfd45dce3dfd3bfb0b73e759a
thiago.alv.santos <thiago.alv.santos@0e5ddfc4-2d14-11df-bc59-2ffd2b2536e4>
2010-10-05 01:44:39 +0000

    Ajustamentos....

---
+++ Desenvolvimento/Fontes/sabrh/sabrh-entity/src/main/java/br/pucpr/sabrh/entity/TipoStatusFemea.java
@@ -0,0 +1,23 @@
+/*
+ * Todos os direitos reservados a TR Consulting.
+ */
+package br.pucpr.sabrh.entity;
+
+/**
+ * Enum TipoStatus.
+ */
+public enum TipoStatusFemea {
+
+	/** O atributo LACTACAO. */
+	LACTACAO,
+
+	/** O atributo GESTACAO. */
+	GESTACAO,
+
+	/** O atributo AMAMENTACAO. */
+	AMAMENTACAO,
+
+	/** O atributo DESCARTE. */
+	DESCARTE;
+
+}
